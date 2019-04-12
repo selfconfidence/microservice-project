8128d9063a48bd24c91172aac16935365f517d9c
misterWei <mynameisweiyan@gmail.com>
2019-04-13 01:30:48 +0800

    控制点赞数以及添加评论父节点增加评论 -author weiyan

--- microservice_parent/microservice_spit/src/main/java/ml/weiyan/spit/controller/SpitController.java
+++ microservice_parent/microservice_spit/src/main/java/ml/weiyan/spit/controller/SpitController.java
@@ -7,6 +7,7 @@ import ml.weiyan.spit.pojo.Spit;
 import ml.weiyan.spit.service.SpitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -20,6 +21,9 @@ import org.springframework.web.bind.annotation.*;
 public class SpitController {
     @Autowired
     private SpitService spitService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity findAll(){
@@ -41,9 +45,22 @@ public class SpitController {
         return new ResponseEntity(true, ResponseCode.OK,"删除成功!");
     }
 
-    @RequestMapping(value = "/search/{parentid}/{page}/{size}",method = RequestMethod.GET)
+    @RequestMapping(value = "/comment/{parentid}/{page}/{size}",method = RequestMethod.GET)
     public ResponseEntity findPage(@PathVariable("parentid") String parentid,@PathVariable("page") int page,@PathVariable("size") int size){
         Page<Spit> parentAndPage = spitService.findParentAndPage(parentid, page, size);
         return new ResponseEntity(true,ResponseCode.OK,"查询成功!",new ResponsePageEntity<>(parentAndPage.getTotalElements(),parentAndPage.getContent()));
+    }
+    @RequestMapping(value = "/thumbup/{spitid}",method = RequestMethod.PUT)
+   public ResponseEntity thumbup(@PathVariable("spitid") String spitid){
+        //控制点赞数目,同一个用户不能对多个评论点赞.使用redis
+         String userId = "123456";
+        if (redisTemplate.opsForValue().get("thumbup_"+userId+spitid) != null) {
+            return new ResponseEntity(false,ResponseCode.REMOTEERROR,"不可点赞!");
+
+        }
+        spitService.thumbup(spitid);
+        redisTemplate.opsForValue().set("thumbup_"+userId+spitid,spitid);
+        return new ResponseEntity(true,ResponseCode.OK,"点赞成功!");
+
     }
 }
--- microservice_parent/microservice_spit/src/main/java/ml/weiyan/spit/service/SpitService.java
+++ microservice_parent/microservice_spit/src/main/java/ml/weiyan/spit/service/SpitService.java
@@ -6,9 +6,15 @@ import ml.weiyan.utils.IdWorker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -25,11 +31,28 @@ public class SpitService {
    @Autowired
     IdWorker idWorker;
 
+   @Autowired
+   private MongoTemplate mongoTemplate;
+
    public List<Spit> findAll(){
        return spitDao.findAll();
    }
    public void save(Spit spit){
        spit.set_id(idWorker.nextId());
+       spit.setPublishtime(new Date());
+       spit.setComment(0);
+       spit.setThumbup(0);
+       spit.setVisits(0);
+       spit.setComment(0);
+       spit.setState("1");
+       //查看是否有父节点,有的话需要对父节点进行增加评论数操作
+       if (!StringUtils.isEmpty(spit.getParentid())) {
+           Update update = new Update();
+           Query query = new Query();
+           query.addCriteria(Criteria.where("_id").is(spit.getParentid()));
+           update.inc("comment",1);
+           mongoTemplate.updateFirst(query,update,"spit");
+       }
        spitDao.save(spit);
    }
 
@@ -41,6 +64,15 @@ public class SpitService {
 
    public Page<Spit> findParentAndPage(String parentId,int page,int size){
 
-       return spitDao.findByParentid(parentId, PageRequest.of(page, size));
+       return spitDao.findByParentid(parentId, PageRequest.of(page-1, size));
    }
+
+    public void thumbup(String spitid) {
+       //初始化点赞数 使用mongo原生的语法进行初始化点赞数
+        Update update = new Update();
+        Query query = new Query();
+        query.addCriteria(Criteria.where("_id").is(spitid));
+        update.inc("thumbup",1);
+     mongoTemplate.updateFirst(query,update,"spit");
+    }
 }
