20812b2e5cac0d50765f0e5bd42d50290ea13647
selfconfidence <mynameisweiyan@gmail.com>
2019-04-15 09:29:58 +0800

    短信返回值完善 -author weiyan

--- microservice_parent/microservice_sendmessage/src/main/java/ml/weiyan/sendmessage/controller/SendController.java
+++ microservice_parent/microservice_sendmessage/src/main/java/ml/weiyan/sendmessage/controller/SendController.java
@@ -1,11 +1,14 @@
 package ml.weiyan.sendmessage.controller;
 
+import com.aliyuncs.exceptions.ClientException;
 import ml.weiyan.result.ResponseCode;
 import ml.weiyan.result.ResponseEntity;
 import ml.weiyan.sendmessage.service.SendService;
 import ml.weiyan.utils.SendMessages6Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 /**
  * @author mister_wei
@@ -25,8 +28,8 @@ public class SendController {
     private SendMessages6Util sendMessages6Util;
 
     @RequestMapping(value = "/message/{phone}",method = RequestMethod.GET)
-    public ResponseEntity sendMessage(@PathVariable("phone") String phone){
-        String result = sendService.sendMessage(sendMessages6Util.smsMessageRandom(), phone);
-      return new ResponseEntity(true, ResponseCode.OK,"发送成功!",result);
+    public ResponseEntity sendMessage(@PathVariable("phone") String phone) throws ClientException {
+        Map result = sendService.sendMessage(sendMessages6Util.smsMessageRandom(), phone);
+      return new ResponseEntity(true, ResponseCode.OK,"发送成功!",result.get("code"));
     }
 }
